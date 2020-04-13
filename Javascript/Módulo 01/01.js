7baa42ead745d3230368b513f6e17e8ffee17a2f
stenio-moreira <steniomoreira01@gmail.com>
2020-04-13 16:58:24 -0300

    Resolução exercício 01 módulo 01

---
+++ Javascript/Módulo 01/01.js
@@ -0,0 +1,18 @@
+var endereco = {
+    rua: "Rua dos pinheiros",
+    numero: 1293,
+    bairro: "Centro",
+    cidade: "São Paulo",
+    uf: "SP"
+};
+
+function getEndereco(endereco) {
+    return 'O usuário mora em '
+    + endereco.cidade + ' / '
+    + endereco.uf +', no bairro '
+    +endereco.bairro+', na rua '
+    +endereco.rua+' "Rua dos Pinheiros" com nº'
+    +endereco.numero;
+}
+
+console.log(getEndereco(endereco));
